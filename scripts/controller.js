ff89274a9df8a54ca9819d67678f84f11976ac98
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2023-03-10 12:09:53 +0700

    controller.js - delete button
    - Fix: delete button unresponsive on touch screens.
    - Enhancement: now show project name when you delete a project

--- scripts/controller.js
+++ scripts/controller.js
@@ -840,25 +840,23 @@ function Controller() {
                         var _name = results[i]['name'].replace(/\#\|\#/g, "'");
                         _name = _name.replace(/\#\|\|\#/g, '"');
                         temp_html += '<div class="btn-group mb-2" role="group" aria-label="Project lists"><button type="button" class="text-start btn btn-outline-secondary create_project" data-key="' + results[i]['data_id'] + '" data-project="' + results[i]['name'] + '" data-type="save">' + _name + '<br>' + results[i]['dtime'] + '</button>';
-                        temp_html += '<button type="button" class="btn btn-outline-danger delete_project" data-key="' + results[i]['id'] + '" data-bs-toggle="modal" data-bs-target="#deleteModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></button></div>';
+                        temp_html += '<button type="button" class="btn btn-outline-danger delete_project" data-key="' + results[i]['id'] + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></button></div>';
                     }
                     temp_html += '</div>';
                     head_html += temp_html;
-                    //$('.second_page_body_right').show().empty().html(head_html);
+
                     $('#newProjectModal').modal('show').find('.modal-body').html(head_html);
                     $('#newProjectModalLabel').text(_this.project_xml_data[_this.current_key]['project_name']);
                     $('.delete_project').unbind(event_type).bind(event_type, function(e2) {
                         e2.preventDefault();
+                        $('#newProjectModal').modal('hide');
+                        $('#deleteModal').modal('show');
+                        $('#deleteComplete').hide();
+                        $('#deleteProjName').text('Do you want to delete “' + $(this).parent().find('[data-project]').attr('data-project') + '”? This action cannot be undone.');
                         var delete_id = $(this).attr('data-key');
                         var temp_this = this;
-                        //var r = confirm("Are you sure you want to delete this project?");
-                        //$('.white_content').remove();
-                        //var temp_pop = '<div class="white_content"><div class="pop_wrap"><div class="pop_msg">Are you sure you want to delete this project?</div><div class="btn_wrp"><div class="can_btn_pop">Cancel</div><div class="ok_btn_pop">OK</div></div></div></div>';
-                        //$('.main_wrapper').append(temp_pop);
-                        //$('.white_content').show();
-                        //call_pop();
                         $('.ok_btn_delete').off(event_type).on(event_type, function() {
-                            //$('.white_content').hide();
+
                             async function removeProj() {
                                 if (/academic/.test(location.pathname) == !1) {
                                     var deleteProj = await connection.remove({
@@ -878,6 +876,8 @@ function Controller() {
                                 
                             };
                             removeProj();
+                            $('#deleteComplete').show();
+                            $('#confirmDelete').hide();
                             $(temp_this).prev().remove();
                             (temp_this).remove();
                         });
