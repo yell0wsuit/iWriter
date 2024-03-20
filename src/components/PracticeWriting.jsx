665c549a32efb3f816f38c904db2353e74508c45
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-03-20 18:45:40 +0700

    Add warning when loading project, increased toast noti. duration

--- src/components/PracticeWriting.jsx
+++ src/components/PracticeWriting.jsx
@@ -282,38 +282,53 @@ function PracticeWriting({ folder, file, data, setHasUnsavedChanges, paragraphsD
                 </Modal.Header>
                 <Modal.Body>
                     {projectsForLocation.length > 0 ? (
-                        <ListGroup className="mb-3">
-                            {projectsForLocation.map((project) => (
-                                <ListGroup.Item className="d-flex justify-content-between align-items-center" key={project.id}>
-                                    <div className="me-auto">
-                                        <div>
-                                            <Button variant="link" className="p-0" onClick={() => loadProject(project.id)}>
-                                                {project.projectName}
-                                            </Button>
+                        <>
+                            <ListGroup className="mb-3">
+                                {projectsForLocation.map((project) => (
+                                    <ListGroup.Item className="d-flex justify-content-between align-items-center" key={project.id}>
+                                        <div className="me-auto">
+                                            <div>
+                                                <Button variant="link" className="p-0" onClick={() => loadProject(project.id)}>
+                                                    {project.projectName}
+                                                </Button>
+                                            </div>
+                                            <div className="">
+                                                {new Date(project.date).toLocaleString("en-US", {
+                                                    month: "long",
+                                                    day: "numeric",
+                                                    year: "numeric",
+                                                    hour: "2-digit",
+                                                    minute: "2-digit",
+                                                    second: "2-digit",
+                                                })}
+                                            </div>
                                         </div>
-                                        <div className="">
-                                            {new Date(project.date).toLocaleString("en-US", {
-                                                month: "long",
-                                                day: "numeric",
-                                                year: "numeric",
-                                                hour: "2-digit",
-                                                minute: "2-digit",
-                                                second: "2-digit",
-                                            })}
-                                        </div>
-                                    </div>
-                                    <Button
-                                        variant="outline-danger"
-                                        size="sm"
-                                        onClick={() => {
-                                            setShowDeleteConfirm(true);
-                                            setProjectToDelete({ id: project.id, name: project.projectName });
-                                        }}>
-                                        Delete
-                                    </Button>
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
+                                        <Button
+                                            variant="outline-danger"
+                                            size="sm"
+                                            onClick={() => {
+                                                setShowDeleteConfirm(true);
+                                                setProjectToDelete({ id: project.id, name: project.projectName });
+                                            }}>
+                                            Delete
+                                        </Button>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                            <p className="text-danger mb-0">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    className="bi bi-exclamation-triangle me-1"
+                                    viewBox="0 0 16 16">
+                                    <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+                                    <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                                </svg>
+                                Remember to save your current work—if needed—before loading.
+                            </p>
+                        </>
                     ) : (
                         <ListGroup>
                             <ListGroup.Item disabled>No project saved</ListGroup.Item>
@@ -342,7 +357,7 @@ function PracticeWriting({ folder, file, data, setHasUnsavedChanges, paragraphsD
                 </Modal.Footer>
             </Modal>
             <ToastContainer className="p-3 position-fixed bottom-0 start-50 translate-middle-x">
-                <Toast className="text-bg-secondary" onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
+                <Toast className="text-bg-secondary" onClose={() => setShowToast(false)} show={showToast} delay={5000} autohide>
                     <Toast.Body>{toastMessage}</Toast.Body>
                 </Toast>
             </ToastContainer>
