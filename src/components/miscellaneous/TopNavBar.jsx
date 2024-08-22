87cf4c0d03a5d30fb81a53312892d87311f60da9
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-08-22 10:05:40 +0700

    click on the logo won't do a full page reload

--- src/components/miscellaneous/TopNavBar.jsx
+++ src/components/miscellaneous/TopNavBar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
@@ -7,7 +6,7 @@ const TopNavBar = () => {
         <div className="mb-4">
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand as={NavLink} to="/">
                         <img alt="" src="/images/icons/ios/32.png" className="d-inline-block align-top" /> Oxford iWriter
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="top-NavBar" />
